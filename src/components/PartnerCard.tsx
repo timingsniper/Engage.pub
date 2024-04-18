62f332525be40a807c37e4bfa8a4557f61b1211e
Percy Jang <joonwoo_jang@pku.edu.cn>
2024-04-19 02:36:06 +0800

    #11 fix: Partner card fix

--- src/components/PartnerCard.tsx
+++ src/components/PartnerCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 type CardProps = {
@@ -17,12 +18,12 @@ export default function PartnerCard({
     <div className="card w-5/6 bg-base-100 shadow-xl">
       <figure>
         <picture>
-          <img src={imgSrc} alt="Shoes" />
+          <Image src={imgSrc} alt="Scenario Image" width={300} height={300} />
         </picture>
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
-        <p>{description}</p>
+        <p className="line-clamp-3">{description}</p>
         <div className="justify-start text-sm">{`#${id}`}</div>
         <div className="card-actions justify-end">
           <Link href={`/scenario/${id}`} passHref>
